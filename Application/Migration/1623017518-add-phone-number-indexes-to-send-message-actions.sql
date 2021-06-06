edd50a71bfeb82bbb127f5846865ae44ad73e977
John Yoder <john.yoder@appfolio.com>
2021-06-06 16:03:56 -0700

    Remove existing incorrect indexes

--- Application/Migration/1623017518-add-phone-number-indexes-to-send-message-actions.sql
+++ Application/Migration/1623017518-add-phone-number-indexes-to-send-message-actions.sql
@@ -1,2 +1,5 @@
+DROP INDEX send_message_actions_from_id_index;
+DROP INDEX send_message_actions_to_id_index;
+
 CREATE INDEX send_message_actions_from_id_index ON send_message_actions (from_id);
 CREATE INDEX send_message_actions_to_id_index ON send_message_actions (to_id);
