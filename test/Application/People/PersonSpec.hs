03e08ed41842bdcd88742d711dbd3c5bf08a6c1f
John Yoder <john.yoder@appfolio.com>
2021-07-23 17:48:26 -0700

    Add missing unit tests to Person module

---
+++ test/Application/People/PersonSpec.hs
@@ -0,0 +1,101 @@
+module Application.People.PersonSpec where
+
+import qualified Application.People.Person as Person
+import Generated.Types
+import IHP.ControllerPrelude
+import IHP.Test.Mocking
+import Test.Hspec
+import Tests.Support
+
+spec :: Spec
+spec = do
+    describe "fetchBotId" do
+        beforeAll (testConfig >>= mockContext RootApplication) do
+            itIO "selects the bot id" do
+                bot <-
+                    newRecord @Person
+                        |> set #firstName "Tim"
+                        |> set #lastName "Eckard"
+                        |> set #goesBy "Tim the Bot"
+                        |> createRecord
+
+                newRecord @Person
+                    |> set #firstName "Jim"
+                    |> set #lastName "Neckard"
+                    |> set #goesBy "I'm a real boy"
+                    |> createRecord
+
+                botId <- Person.fetchBotId
+                botId `shouldBe` get #id bot
+
+    describe "fetchByPhoneNumber" do
+        beforeAll (testConfig >>= mockContext RootApplication) do
+            itIO "selects a person that corresponds to a given phone number" do
+                jim <-
+                    newRecord @Person
+                        |> set #firstName "Jim"
+                        |> set #lastName "Neckard"
+                        |> set #goesBy "Jim"
+                        |> createRecord
+
+                jimPhoneNumber <-
+                    newRecord @PhoneNumber
+                        |> set #number "+14444444444"
+                        |> createRecord
+
+                newRecord @PhoneContact
+                    |> set #personId (get #id jim)
+                    |> set #phoneNumberId (get #id jimPhoneNumber)
+                    |> createRecord
+
+                sally <-
+                    newRecord @Person
+                        |> set #firstName "Sally"
+                        |> set #lastName "Smith"
+                        |> set #goesBy "Sally"
+                        |> createRecord
+
+                sallyPhoneNumber <-
+                    newRecord @PhoneNumber
+                        |> set #number "+15555555555"
+                        |> createRecord
+
+                newRecord @PhoneContact
+                    |> set #personId (get #id sally)
+                    |> set #phoneNumberId (get #id sallyPhoneNumber)
+                    |> createRecord
+
+                person <- Person.fetchByPhoneNumber $ get #id jimPhoneNumber
+                get #id person `shouldBe` get #id jim
+
+    describe "botGoesBy" do
+        it "returns Tim the Bot" do
+            Person.botGoesBy `shouldBe` "Tim the Bot"
+
+    describe "validate" do
+        it "ensures that the first name is non-empty" do
+            let person =
+                    newRecord @Person
+                        |> set #firstName ""
+                        |> set #lastName "Rogers"
+                        |> set #goesBy "Mister"
+            Person.validate person |> get #meta |> get #annotations
+                `shouldBe` [("firstName", "This field cannot be empty")]
+
+        it "ensures that the last name is non-empty" do
+            let person =
+                    newRecord @Person
+                        |> set #firstName "Fred"
+                        |> set #lastName ""
+                        |> set #goesBy "Mister"
+            Person.validate person |> get #meta |> get #annotations
+                `shouldBe` [("lastName", "This field cannot be empty")]
+
+        it "ensures that the goes-by field is non-empty" do
+            let person =
+                    newRecord @Person
+                        |> set #firstName "Fred"
+                        |> set #lastName "Rogers"
+                        |> set #goesBy ""
+            Person.validate person |> get #meta |> get #annotations
+                `shouldBe` [("goesBy", "This field cannot be empty")]
