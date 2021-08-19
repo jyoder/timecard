0ee854d5ac63dc0009e3eddcd53d3fa45bb512c4
John Yoder <john.yoder@appfolio.com>
2021-08-18 17:22:43 -0700

    Add unit tests for view time module

--- Web/View/Service/Time.hs
+++ Web/View/Service/Time.hs
@@ -2,15 +2,11 @@ module Web.View.Service.Time (
     formatDay,
     formatTimeOfDay,
     formatDateTime,
-    weekday,
 ) where
 
 import Data.Text (pack, strip)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 import Data.Time.Format.ISO8601 (iso8601Show)
-import IHP.View.TimeAgo as TO
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
 import Web.View.Prelude
 
 formatDay :: Day -> Text
@@ -22,16 +18,3 @@ formatTimeOfDay timeOfDay =
 
 formatDateTime :: UTCTime -> Text
 formatDateTime time = strip $ pack $ formatTime defaultTimeLocale "%FT%X%z" time
-
-weekday :: UTCTime -> Html
-weekday = timeElement "weekday"
-
-timeElement :: Text -> UTCTime -> Html
-timeElement className dateTime =
-    H.time
-        ! A.class_ (cs className)
-        ! A.datetime (cs $ iso8601Show dateTime)
-        $ cs (beautifyUtcTime dateTime)
-
-beautifyUtcTime :: UTCTime -> String
-beautifyUtcTime = formatTime defaultTimeLocale "%d.%m.%Y, %H:%M"
---
+++ test/Web/View/Service/TimeSpec.hs
@@ -0,0 +1,20 @@
+module Web.View.Service.TimeSpec where
+
+import IHP.Prelude
+import Test.Hspec
+import Tests.Support
+import Web.View.Service.Time
+
+spec :: Spec
+spec = do
+    describe "formatDay" do
+        it "returns a string with 'mm/dd/yyyy' format" do
+            formatDay (toDay "2021-08-01") `shouldBe` "08/01/2021"
+
+    describe "formatTimeOfDay" do
+        it "returns a string with 'h:mm AM/PM' format" do
+            formatTimeOfDay (toTimeOfDay "07:03:01") `shouldBe` "7:03 AM"
+
+    describe "formatDateTime" do
+        it "returns a string with 'yyyy-mm-ddThh:mm:ss+tz' format" do
+            formatDateTime (toUtc "2021-06-01 15:00:00 PDT") `shouldBe` "2021-06-01T22:00:00+0000"
