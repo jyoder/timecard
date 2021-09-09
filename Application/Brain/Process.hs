206fd7bddc11c84e3f101030c884d636769c5a60
John Yoder <john.yoder@appfolio.com>
2021-09-08 23:39:30 -0700

    Remove obsolete process functions

--- Application/Brain/Process.hs
+++ Application/Brain/Process.hs
@@ -1,7 +1,5 @@
 module Application.Brain.Process (
     processIncomingMessage,
-    processState,
-    suspendSendMessageActionsAfter,
 ) where
 
 import qualified Application.Action.ActionRunState as ActionRunState
@@ -34,37 +32,3 @@ processIncomingMessage twilioMessage = do
             let plan = Decide.decide situation
             Act.act plan
         [] -> pure ()
-
-processState :: (?modelContext :: ModelContext) => Id PhoneNumber -> IO ()
-processState phoneNumberId = do
-    twilioMessages <-
-        query @TwilioMessage
-            |> filterWhere (#fromId, phoneNumberId)
-            |> orderByDesc #createdAt
-            |> limit 1
-            |> fetch
-
-    case twilioMessages of
-        twilioMessage : messages ->
-            suspendSendMessageActionsAfter
-                phoneNumberId
-                (get #createdAt twilioMessage)
-        [] -> pure ()
-
-suspendSendMessageActionsAfter ::
-    (?modelContext :: ModelContext) =>
-    Id PhoneNumber ->
-    UTCTime ->
-    IO ()
-suspendSendMessageActionsAfter phoneNumberId time = do
-    sendMessageActions <-
-        SendMessageAction.fetchNotStartedCreatedBeforeByPhoneNumber
-            time
-            phoneNumberId
-
-    mapM_
-        ( \action -> do
-            actionRunState <- fetch $ get #actionRunStateId action
-            ActionRunState.updateSuspended actionRunState
-        )
-        sendMessageActions
