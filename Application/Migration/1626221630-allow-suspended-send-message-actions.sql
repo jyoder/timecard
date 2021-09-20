9bd30fe1f35ea358e3a5fc9cf8ec9af7a8930e68
John Yoder <john.yoder@appfolio.com>
2021-09-19 20:32:29 -0700

    Remove old migrations

--- Application/Migration/1626221630-allow-suspended-send-message-actions.sql
+++
@@ -1,6 +0,0 @@
-alter table "action_run_states"
-    drop constraint "action_run_states_state_check";
-
-alter table "action_run_states"
-    add constraint "action_run_states_state_check" check (((state = 'not_started'::text) or (state = 'suspended'::text) or (state = 'running'::text) or (state = 'canceled'::text) or (state = 'finished'::text) or (state = 'failed'::text)));
-
--- Application/Migration/1626933667-add-clock-in-out-fields.sql
+++
@@ -1,9 +0,0 @@
-alter table "public"."timecard_entries"
-    add column "clocked_in_at" time without time zone;
-
-alter table "public"."timecard_entries"
-    add column "clocked_out_at" time without time zone;
-
-alter table "public"."timecard_entries"
-    add column "lunch_duration" integer;
-
