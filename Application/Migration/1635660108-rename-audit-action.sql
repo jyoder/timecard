b1f0eb342c4e8e5420c001f9a30c6ec52f1d3311
John Yoder <john.yoder@appfolio.com>
2021-10-31 00:07:42 -0700

    Fix audit_actions migration

--- Application/Migration/1635660108-rename-audit-action.sql
+++ Application/Migration/1635660108-rename-audit-action.sql
@@ -1,25 +1,2 @@
-alter type audit_actions rename to audit_actions__old_version_to_be_dropped;
-
-create type audit_actions as enum (
-    'message_sent',
-    'message_received',
-    'message_processed',
-    'timecard_entry_created',
-    'timecard_entry_edited',
-    'timecard_entry_deleted',
-    'review_link_generated',
-    'review_signed',
-    'daily_reminder_scheduled',
-    'review_request_scheduled',
-    'scheduled_message_edited',
-    'scheduled_message_suspended',
-    'scheduled_message_resumed',
-    'scheduled_message_canceled'
-);
-
-alter table audit_entries
-    alter column action type audit_actions
-    using action::text audit_actions;
-
-drop type audit_actions__old_version_to_be_dropped;
+alter type audit_actions rename value 'scheduled_message_deleted' to 'scheduled_message_canceled';
 
