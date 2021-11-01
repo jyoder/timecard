0169cef78e87aeba69f4bfceca9bfed33adb75f5
John Yoder <john.yoder@appfolio.com>
2021-11-01 10:34:19 -0700

    Remove unnecessary imports

--- Web/Controller/AuditEntries.hs
+++ Web/Controller/AuditEntries.hs
@@ -1,10 +1,7 @@
 module Web.Controller.AuditEntries where
 
 import Web.Controller.Prelude
-import Web.View.AuditEntries.Edit
 import Web.View.AuditEntries.Index
-import Web.View.AuditEntries.New
-import Web.View.AuditEntries.Show
 
 instance Controller AuditEntriesController where
     action AuditEntriesAction = do
