f8df1e6bc0c83260cab0b8f85344dfc262e2d44c
John Yoder <john.yoder@appfolio.com>
2021-11-22 13:12:17 -0800

    Add query for automation report

---
+++ Application/Reports/AutomationQuery.hs
@@ -0,0 +1,150 @@
+module Application.Reports.AutomationQuery (
+    Row (..),
+    AutomationStatus (..),
+    ReportInterval (..),
+    fetch,
+) where
+
+import "string-interpolate" Data.String.Interpolate (i)
+import Database.PostgreSQL.Simple (Only (..), Query)
+import Database.PostgreSQL.Simple.FromField (FromField, ResultError (..), fromField, returnError)
+import Database.PostgreSQL.Simple.FromRow (FromRow, field, fromRow)
+import qualified Generated.Types as Types
+import IHP.ModelSupport
+import IHP.Prelude
+import Text.Read (read)
+
+data Row = Row
+    { weekOf :: !Day
+    , personId :: !(Id Types.Person)
+    , personFirstName :: !Text
+    , personLastName :: !Text
+    , automationStatus :: AutomationStatus
+    }
+    deriving (Eq, Show)
+
+data AutomationStatus
+    = FullyAutomated
+    | NotFullyAutomated
+    | NoActivity
+    deriving (Eq, Show)
+
+data ReportInterval
+    = ByDay
+    | ByWeek
+    deriving (Eq, Show)
+
+instance FromRow Row where
+    fromRow =
+        Row
+            <$> field
+            <*> field
+            <*> field
+            <*> field
+            <*> field
+
+instance FromField AutomationStatus where
+    fromField field maybeData =
+        case maybeData of
+            Just "fully_automated" -> pure FullyAutomated
+            Just "not_fully_automated" -> pure NotFullyAutomated
+            Just "no_activity" -> pure NoActivity
+            Just _data -> returnError ConversionFailed field (cs _data)
+            Nothing -> returnError UnexpectedNull field ""
+
+fetch :: (?modelContext :: ModelContext) => ReportInterval -> Day -> IO [Row]
+fetch reportInterval asOf = sqlQuery (query reportInterval) (Only asOf)
+
+query :: ReportInterval -> Query
+query reportInterval =
+    [i|
+        #{datesSql reportInterval},
+        people_by_date as (
+            select
+                dates.date date,
+                people.id id,
+                people.first_name first_name,
+                people.last_name last_name
+            from
+                dates cross join
+                    people
+        ),
+        automations as (
+            select
+                people.id person_id,
+                #{auditEntryDateSql reportInterval} audit_entry_date,
+                audit_entries.user_id audit_entry_user_id
+            from
+                people,
+                phone_numbers,
+                phone_contacts,
+                audit_entries
+            where
+                phone_contacts.person_id = people.id
+                and phone_contacts.phone_number_id = phone_numbers.id
+                and audit_entries.phone_number_id = phone_numbers.id
+        )
+        select
+            people_by_date.date,
+            people_by_date.id,
+            people_by_date.first_name,
+            people_by_date.last_name,
+            (case
+                when count(automations.person_id) > 0
+                    then
+                        (case
+                            when every(automations.audit_entry_user_id is null)
+                                then 'fully_automated'
+                                else 'not_fully_automated'
+                        end)
+                    else
+                        'no_activity'
+            end) automation_status
+        from
+            people_by_date left join
+                automations on (
+                    people_by_date.id = automations.person_id
+                    and people_by_date.date = automations.audit_entry_date
+                )
+        group by
+            people_by_date.date,
+            people_by_date.first_name,
+            people_by_date.last_name,
+            people_by_date.id
+        order by
+            people_by_date.date desc,
+            people_by_date.last_name asc,
+            people_by_date.first_name asc,
+            people_by_date.id asc;
+    |]
+
+datesSql :: ReportInterval -> Text
+datesSql reportInterval =
+    [i|
+        with dates as (
+            select
+                date::date
+            from
+                generate_series('#{show startDate}', ?, '#{show interval} day'::interval) date
+        )
+    |]
+  where
+    interval = case reportInterval of
+        ByDay -> 1
+        ByWeek -> 7
+
+auditEntryDateSql :: ReportInterval -> Text
+auditEntryDateSql reportInterval =
+    case reportInterval of
+        ByDay ->
+            [i|
+                audit_entries.created_at::date
+            |]
+        ByWeek ->
+            [i|
+                audit_entries.created_at::date - ((6 + cast(extract(dow from audit_entries.created_at::date) as int)) % 7)
+            |]
+
+-- This is the date when we first began collecting audit information.
+startDate :: Day
+startDate = read "2021-10-25"
---
+++ Tests/Application/Reports/AutomationQuerySpec.hs
@@ -0,0 +1,298 @@
+module Tests.Application.Reports.AutomationQuerySpec where
+
+import qualified Application.Reports.AutomationQuery as Reports.AutomationQuery
+import Generated.Types
+import IHP.ControllerPrelude
+import IHP.Prelude
+import Test.Hspec
+import Tests.Support
+
+spec :: Spec
+spec = do
+    aroundAll (withApp RootApplication testConfig) do
+        describe "fetch" do
+            itIO "returns rows to indicate the automation status of each crew member each day" do
+                user <-
+                    newRecord @User
+                        |> set #email "nar@whal.com"
+                        |> set #passwordHash "password"
+                        |> createRecord
+
+                person1 <-
+                    newRecord @Person
+                        |> set #firstName "Bob"
+                        |> set #lastName "Builder"
+                        |> set #goesBy "Bob the Builder"
+                        |> createRecord
+
+                phoneNumber1 <-
+                    newRecord @PhoneNumber
+                        |> set #number "+15555555555"
+                        |> createRecord
+
+                phoneContact1 <-
+                    newRecord @PhoneContact
+                        |> set #personId (get #id person1)
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> createRecord
+
+                auditEntry1 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:00 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 1"
+                        |> createRecord
+
+                auditEntry2 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:01 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> set #action MessageReceived
+                        |> set #actionContext "Entry 2"
+                        |> createRecord
+
+                phoneNumber2 <-
+                    newRecord @PhoneNumber
+                        |> set #number "+16666666666"
+                        |> createRecord
+
+                person2 <-
+                    newRecord @Person
+                        |> set #firstName "Rob"
+                        |> set #lastName "Destroyer"
+                        |> set #goesBy "Rob the Destroyer"
+                        |> createRecord
+
+                phoneContact2 <-
+                    newRecord @PhoneContact
+                        |> set #personId (get #id person2)
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> createRecord
+
+                auditEntry3 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:02 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> set #userId (Just $ get #id user)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 3"
+                        |> createRecord
+
+                auditEntry4 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-31 07:00:02 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 4"
+                        |> createRecord
+
+                rows <- Reports.AutomationQuery.fetch Reports.AutomationQuery.ByDay (toDay "2021-10-30")
+                rows
+                    `shouldBe` [ Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-30"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.FullyAutomated
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-30"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NotFullyAutomated
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-29"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-29"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-28"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-28"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-27"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-27"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-26"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-26"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-25"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-25"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               ]
+
+            itIO "returns rows to indicate the automation status of each crew member each week" do
+                user <-
+                    newRecord @User
+                        |> set #email "nar@whal.com"
+                        |> set #passwordHash "password"
+                        |> createRecord
+
+                person1 <-
+                    newRecord @Person
+                        |> set #firstName "Bob"
+                        |> set #lastName "Builder"
+                        |> set #goesBy "Bob the Builder"
+                        |> createRecord
+
+                phoneNumber1 <-
+                    newRecord @PhoneNumber
+                        |> set #number "+15555555555"
+                        |> createRecord
+
+                phoneContact1 <-
+                    newRecord @PhoneContact
+                        |> set #personId (get #id person1)
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> createRecord
+
+                auditEntry1 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:00 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 1"
+                        |> createRecord
+
+                auditEntry2 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:01 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber1)
+                        |> set #action MessageReceived
+                        |> set #actionContext "Entry 2"
+                        |> createRecord
+
+                phoneNumber2 <-
+                    newRecord @PhoneNumber
+                        |> set #number "+16666666666"
+                        |> createRecord
+
+                person2 <-
+                    newRecord @Person
+                        |> set #firstName "Rob"
+                        |> set #lastName "Destroyer"
+                        |> set #goesBy "Rob the Destroyer"
+                        |> createRecord
+
+                phoneContact2 <-
+                    newRecord @PhoneContact
+                        |> set #personId (get #id person2)
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> createRecord
+
+                auditEntry3 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-30 07:00:02 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> set #userId (Just $ get #id user)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 3"
+                        |> createRecord
+
+                auditEntry4 <-
+                    newRecord @AuditEntry
+                        |> set #createdAt (toUtc "2021-10-31 07:00:02 PDT")
+                        |> set #phoneNumberId (get #id phoneNumber2)
+                        |> set #action MessageSent
+                        |> set #actionContext "Entry 4"
+                        |> createRecord
+
+                rows <- Reports.AutomationQuery.fetch Reports.AutomationQuery.ByWeek (toDay "2021-11-08")
+                rows
+                    `shouldBe` [ Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-11-08"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-11-08"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-11-01"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-11-01"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NoActivity
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-25"
+                                    , personId = get #id person1
+                                    , personFirstName = "Bob"
+                                    , personLastName = "Builder"
+                                    , automationStatus = Reports.AutomationQuery.FullyAutomated
+                                    }
+                               , Reports.AutomationQuery.Row
+                                    { weekOf = toDay "2021-10-25"
+                                    , personId = get #id person2
+                                    , personFirstName = "Rob"
+                                    , personLastName = "Destroyer"
+                                    , automationStatus = Reports.AutomationQuery.NotFullyAutomated
+                                    }
+                               ]
